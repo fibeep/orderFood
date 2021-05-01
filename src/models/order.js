16f8b9f5970e8fae8f65cde7d67422b50f0758c4
Salo <salomonco@gmail.com>
2021-05-01 15:32:24 -0500

    edited menu model + started table tests

--- src/models/order.js
+++ src/models/order.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const OrderSchema = new Schema({
-  table: { type: Schema.Types.ObjectId, ref: "Table", required: true },
-  drinks: [{ type: Schema.Types.ObjectId, ref: "Drink" }],
-  food: [{ type: Schema.Types.ObjectId, ref: "Food" }]
-},
-{ timestamps: { createdAt: "created_at" } }
+const OrderSchema = new Schema(
+  {
+    table: { type: Schema.Types.ObjectId, ref: "Table", required: true },
+    drink: { type: String },
+    food: { type: String },
+    // drinks: [{ type: Schema.Types.ObjectId, ref: "Drink" }],
+    // food: [{ type: Schema.Types.ObjectId, ref: "Food" }]
+  },
+  { timestamps: { createdAt: "created_at" } }
 );
 
 const Order = mongoose.model("Order", OrderSchema);
---
+++ src/tests/table.js
@@ -0,0 +1,160 @@
+require("dotenv").config();
+const app = require("../server.js");
+const mongoose = require("mongoose");
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const assert = chai.assert;
+
+const Table = require("../models/table.js");
+const Order = require("../models/order.js");
+
+chai.config.includeStack = true;
+
+const expect = chai.expect;
+const should = chai.should();
+chai.use(chaiHttp);
+
+/**
+ * root level hooks
+ */
+after((done) => {
+  // required because https://github.com/Automattic/mongoose/issues/1251#issuecomment-65793092
+  mongoose.models = {};
+  mongoose.modelSchemas = {};
+  mongoose.connection.close();
+  done();
+});
+
+describe("Message API endpoints", () => {
+  beforeEach((done) => {
+    const sampleTable = new Table({
+      number: "1",
+    });
+
+    const sampleOrder = new Order({
+      drink: "Water",
+      food: "Chicken",
+    });
+
+    sampleTable
+      .save()
+      .then(() => {
+        sampleOrder.table = sampleTable;
+        return sampleOrder.save();
+      })
+      .then(() => {
+        sampleTable.order = sampleOrder;
+        return sampleTable.save();
+      })
+      .then(() => {
+        done();
+      });
+  });
+
+  afterEach((done) => {
+      order = sampleTable.order._id
+    Order.deleteOne({ id=order })
+      .then(() => {
+        return Table.deleteMany({number:["1"]});
+      })
+      .then(() => {
+        done();
+      });
+  });
+
+  it("should load all tables", (done) => {
+    chai
+      .request(app)
+      .get("/messages")
+      .end((err, res) => {
+        if (err) {
+          done(err);
+        }
+        expect(res).to.have.status(200);
+        expect(res.body.messages).to.be.an("array");
+        done();
+      });
+  });
+
+  it("should get one specific table with its order", (done) => {
+    const message = Message.findOne({ title: "mytitle" });
+    chai
+      .request(app)
+      .get(`/messages/${message._id}`)
+      .end((err, res) => {
+        if (err) {
+          done(err);
+        }
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an("object");
+        expect(res.body.title).to.equal("mytitle");
+        done();
+      });
+  });
+
+  it("should create a new table", (done) => {
+    User.findOne({ username: "newuser" }).then((user) => {
+      chai
+        .request(app)
+        .post("/messages")
+        .send({
+          title: "anothertitle",
+          body: "anotherbody",
+          author: user,
+        })
+        .end((err, res) => {
+          if (err) {
+            done(err);
+          }
+          expect(res.body).to.be.an("object");
+          expect(res.body).to.have.property("title", "anothertitle");
+          // check that message is actually inserted into database
+          Message.findOne({ title: "anothertitle" }).then((message) => {
+            expect(message).to.be.an("object");
+            done();
+          });
+        });
+    });
+  });
+
+  it("should update a table number", (done) => {
+    Message.findOne({ title: "mytitle" }).then((message) => {
+      console.log("The message title is: ", message);
+      done();
+      // chai
+      // .request(app)
+      // .put(`/message/${message._id}`)
+      // .send({ title: "somenewtitle" })
+      // .end((err, res) => {
+      //   if (err) {
+      //     done(err);
+      //   }
+      //   expect(res.body).to.be.an("object");
+      //   expect(res.body).to.have.property("title", "somenewtitle");
+
+      //   // check that user is actually inserted into database
+      // Message.findOne({ title: "somenewtitle" }).then((message) => {
+      // expect(message).to.be.an("object");
+      // done();
+      // });
+      // })
+    });
+  });
+
+  // it("should delete a table", (done) => {
+  //   const message = Message.findOne({ title: "mytitle" });
+  //   chai.request(app)
+  //       .delete(`/messages/${message._id}`)
+  //       .end((err, res) => {
+  //           if (err) { done(err) }
+  //           expect(res.body.message).to.equal('Successfully deleted.')
+  //           expect(res.body._id).to.equal(message._id)
+
+  //           // check that user is actually deleted from database
+  //           Message.findOne({title: 'mytitle'}).then(message => {
+  //               expect(message).to.equal(null)
+  //               done()
+  //           })
+  //       })
+  //   })
+});
