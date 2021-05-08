5ff5782e78b6fe828547def94abffcc559068859
Salo <salomonco@gmail.com>
2021-05-08 16:02:31 -0500

    route to get ALL orders works

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-
+const Order = require("../models/order");
 const Table = require("../models/table");
+
+
+// Route to get all orders
+
+router.get('/', (req,res) => {
+    // GET all orders using .find()
+    Order.find()
+    .then((order) =>{
+        // Returns tables as JSON list
+        return res.json({order})
+    })
+    .catch((err)=>{
+        throw err.message
+    })
+})
+
+
 
 module.exports = router;
